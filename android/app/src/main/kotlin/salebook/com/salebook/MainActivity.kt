27ebd61d4c3de34f3d23e78f2d796d2918916143
Fazla Arafat <fazle.arafat@softbakso.com>
2019-10-20 15:19:33 +0600

    Source files

---
+++ android/app/src/main/kotlin/salebook/com/salebook/MainActivity.kt
@@ -0,0 +1,13 @@
+package salebook.com.salebook
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
